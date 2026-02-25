f1d6be5a9f3e7e7a631dcc661ed5f4cdc027c75e
Glack <glack@local>
2026-02-25 19:50:30 +0300

    revert(gumtree-font): remove extra manual bold pass

--- rust/backend/src/generator/gumtree.rs
+++ rust/backend/src/generator/gumtree.rs
@@ -294,8 +294,6 @@ fn draw_text_bold_with_letter_spacing(
     draw_text_with_letter_spacing(img, font, px, x, y, color, text, letter_spacing);
     draw_text_with_letter_spacing(img, font, px, x - 1, y, color, text, letter_spacing);
     draw_text_with_letter_spacing(img, font, px, x + 1, y, color, text, letter_spacing);
-    // tiny extra weight
-    draw_text_with_letter_spacing(img, font, px, x, y + 1, color, text, letter_spacing);
 }
 
 fn overlay_alpha(base: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, over: &ImageBuffer<Rgba<u8>, Vec<u8>>, x: u32, y: u32) {
